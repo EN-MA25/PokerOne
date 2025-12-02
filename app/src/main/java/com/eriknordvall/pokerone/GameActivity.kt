74d8b27c219dace025ec64605fa83c4b8e20fc18
Erik <3kdapp25.norder@folkuniversitetet.nu>
2025-12-02 16:24:14 +0100

    Made the cards selectable

--- app/src/main/java/com/eriknordvall/pokerone/GameActivity.kt
+++ app/src/main/java/com/eriknordvall/pokerone/GameActivity.kt
@@ -1,12 +1,20 @@
 package com.eriknordvall.pokerone
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginBottom
+import androidx.core.view.marginTop
 
 class GameActivity : AppCompatActivity() {
+
+    var cardSelected : BooleanArray = booleanArrayOf(false, false, false, false, false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
@@ -17,5 +25,22 @@ class GameActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+    }
+
+    fun cardPressed(card: View) {
+        Log.d("!!!", "Card ${card.tag} ${card.top} ${card.bottom}, ${card.paddingTop} ${card.paddingBottom}, ${card.marginTop} ${card.marginBottom}")
+
+        val tag: Int = card.tag.toString().toInt()
+
+        if (cardSelected[tag]) {
+            card.offsetTopAndBottom(64)
+            cardSelected[tag] = false
+        } else {
+            card.offsetTopAndBottom(-64)
+            cardSelected[tag] = true
+        }
+
+
+
     }
 }
