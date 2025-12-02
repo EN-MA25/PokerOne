e118f22213f6c67c0a1e05367b5e4b4cd737f438
Erik <3kdapp25.norder@folkuniversitetet.nu>
2025-12-02 14:09:44 +0100

    Added Start Game Screen

--- app/src/main/java/com/eriknordvall/pokerone/GameActivity.kt
+++ app/src/main/java/com/eriknordvall/pokerone/GameActivity.kt
@@ -1,28 +1,21 @@
 package com.eriknordvall.pokerone
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
-    }
-
-    fun startGame(view: View) {
-        val intent = Intent(this, GameActivity::class.java)
-        //intent.putExtra("name", "start")
-        startActivity(intent)
     }
 }
